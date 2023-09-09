4be3d8fbfc26c1e1df801e8fb5128a62e61efda7
Yugeyong Choi <69104743+YUgyeong-Choi@users.noreply.github.com>
2023-09-09 18:38:01 +0900

    프로젝트 생성과 출력

---
+++ InflearnLetMakeGames/InflearnLetMakeGames/main.cpp
@@ -0,0 +1,11 @@
+// #include : 헤더파일을 여기에 포함시키는 기능, 전처리기
+#include <iostream>
+
+//실행 과정 : 컴파일 -> 빌드 -> 실행
+//Ctrl + Shift + B 를 이용해서 컴파일 및 빌드 가능
+
+int main() {
+	std::cout << "Test Output";
+
+	return 0;
+}
